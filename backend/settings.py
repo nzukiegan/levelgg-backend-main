5fca9766cdcb9b4b6c168055c62d3df7786c5fae
nzukiegan <nzukiegan6@gmail.com>
2025-07-11 16:42:57 +0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -138,6 +138,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
+    os.environ.get('CLIENT_URL'),
     os.environ.get('BACKEND_URL2'),
     'http://localhost:3000',
     'http://127.0.0.1:3000',
